ff2af272105a11606b6d5a7b5a7baa1a37392770
Jeff Lunt <jefflunt@gmail.com>
2012-05-26 12:20:57 -0500

    References #7 - Added state methods - keep? - scratch? - neutral?

--- app/models/thought.rb
+++ app/models/thought.rb
@@ -2,5 +2,17 @@ class Thought < ActiveRecord::Base
   belongs_to :thought_wall
   
   validates :text,  :presence => true
-  validates :text,  :uniqueness => { :scope => :thought_wall_id }  
+  validates :text,  :uniqueness => { :scope => :thought_wall_id }
+  
+  def keep?
+    keep == true
+  end
+  
+  def scratch?
+    keep == false
+  end
+  
+  def neutral?
+    keep == nil
+  end
 end
