7729c32657fc6e82067b0479a377afd1b3fe0d86
Jeff Lunt <jefflunt@gmail.com>
2012-05-27 10:00:18 -0500

    Closes #17 - Fixes scrolling issue - The problem here wasn't a post back issue. The problem was that I was returning keyboard focus to the new thought box upon every update, which was causing the page to scroll up in order to make the keyboard cursor visible. This simply wasn't necessary, so I took out the call to focus on thought update.

--- app/views/thoughts/update.js
+++ app/views/thoughts/update.js
@@ -1,3 +1,1 @@
 $('#thought-box-<%= escape_javascript(@thought.id.to_s) %>').replaceWith("<%= escape_javascript(render :partial => 'thoughts/thought_box',  :locals => {:thought => @thought}) %>");
-
-$('#thought_text').focus();
