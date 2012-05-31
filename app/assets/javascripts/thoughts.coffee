d983090512f8d514656edc0e550f3bf8eb5a53d6
Jeff Lunt <jefflunt@gmail.com>
2012-05-31 09:45:14 -0500

    Fixed a bug that was preventing the `remaining:` character count from updating. This happened due to a change in an `id` which wasn't reflected in the relevant JavaScript file. Precompile assets. This version is deployable.

--- app/assets/javascripts/thoughts.coffee
+++ app/assets/javascripts/thoughts.coffee
@@ -10,7 +10,7 @@ $ ->
   $('#thought_text').focus()
   
   $("body").delegate "#thought_text", "keyup", (event) ->
-      $('#thought_new_remaining_characters').html('remaining: ' + (maxThoughtChars - $('#thought_text').val().length))
+      $('#thought-new-remaining-characters').html('remaining: ' + (maxThoughtChars - $('#thought_text').val().length))
         
   $("#thought_wall_title").keyup ->
     $("#update_thought_wall_spinner").show()
--- app/views/thoughts/create.js
+++ app/views/thoughts/create.js
@@ -1,6 +1,9 @@
 $('#next_refresh').html('5000');
 
 $('#thought_text').val('');
+maxThoughtChars = 65500
+$('#thought-new-remaining-characters').html('remaining: ' + (maxThoughtChars - $('#thought_text').val().length))
+
 $('#thought-list').prepend("<%= escape_javascript(render(:partial => 'thoughts/thought_box', :locals => {:thought => @thought, :is_new_thought => true})) %>");
 $('#thought-box-<%= escape_javascript(@thought.id.to_s) %>').animate({width: 215}, 500, function() { $(this).animate({opacity: 1.0}, 175, function() { $(this).removeClass('new-thought-box'); }) });
 
