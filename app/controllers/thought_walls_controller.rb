4e9626a766e0f7ca4014ce1be36dfbba6b4fba7e
Jeff Lunt <jefflunt@gmail.com>
2012-06-02 15:54:47 -0500

    A few tweaks to the refresh timing. - Reduced the minimum refresh to 1500 milliseconds (down from 5000). - Maximum reduced from 10 minutes to 60 seconds. - Slowed the rate at which the update increases, so it takes longer to hit the max (although, now that the max is lower, maybe it doesn't really take any longer). - Fixed issues where edits to the title and thoughts weren't triggering an update refresh on the remote end of the connection. - Refactored several pieces of the timing code to make them shorter. - Other small tweaks to make things a bit more readable.

--- app/controllers/thought_walls_controller.rb
+++ app/controllers/thought_walls_controller.rb
@@ -8,10 +8,15 @@ class ThoughtWallsController < ApplicationController
       @thought_wall = ThoughtWall.find_by_code(params[:id])
       @thoughts = Thought.where(["thought_wall_id = ? AND updated_at >= ?", @thought_wall.id, last_render_at]).order("id DESC")
       
-      @next_refresh = get_next_refresh(params[:next_refresh].to_i, @thoughts.count)
+      @update_client_title = @thought_wall.updated_at > last_render_at
+      if @update_client_title || @thoughts.count > 0
+        @next_refresh = get_next_refresh(0, true)
+      else
+        @next_refresh = get_next_refresh(params[:next_refresh].to_i, false)
+      end
     else
       @thought_wall = ThoughtWall.find_by_code(params[:id], :include => :thoughts)
-      @next_refresh = 5000
+      @next_refresh = get_next_refresh(0, true)
     end
     
     respond_to do |format|
@@ -45,25 +50,14 @@ class ThoughtWallsController < ApplicationController
       new_code 
     end
     
-    def get_next_refresh(next_refresh, updated_thoughts_count)
-      min_refresh = 5000
+    def get_next_refresh(desired_next_refresh, reset_refresh_rate)
+      min_refresh = 1500
+      return min_refresh if reset_refresh_rate
       
-      # Absolute minimum refresh time is 5000 milliseconds
-      next_refresh = next_refresh || min_refresh
-      if next_refresh < min_refresh
-        next_refresh = min_refresh
-      end
+      next_refresh = [desired_next_refresh || min_refresh, min_refresh].max
 
-      # If no new results, extend refresh time to a maximum of 10 minutes
-      if updated_thoughts_count == 0
-        next_refresh_in_seconds = next_refresh / 1000
-        next_refresh_in_seconds = (next_refresh_in_seconds**2)/12
-        next_refresh = [next_refresh + (next_refresh_in_seconds*1000), 600000].min
-      else
-        next_refresh = min_refresh
-      end
-      
-      next_refresh
+      next_refresh_in_seconds = (((next_refresh / 1000.0)**2)/30.0)
+      next_refresh = [next_refresh + (next_refresh_in_seconds*1000).to_i, 60000].min
     end
   
 end
