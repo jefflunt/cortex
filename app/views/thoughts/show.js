859a09b7731b8c6a440280e92134cbbe7c2ebba4
Jeff Lunt <jefflunt@gmail.com>
2012-06-04 10:40:39 -0500

    References #33 - History display now gets hidden when you click away from it.

--- app/views/thoughts/show.js
+++ app/views/thoughts/show.js
@@ -2,6 +2,7 @@ overlayContentDiv.html("<%= escape_javascript(render(:partial => 'thoughts/thoug
 
 overlayDiv.show();
 overlayDiv.animate({opacity: 1.0}, 100);
+hideAllThoughtHistoryBoxes()
 if (recentThoughtWallsDiv.css('opacity') === "1") {
   toggleVisibilityOfRecentThoughtWallsDiv();
 }
