4e3b49b7ac0da1bd9bf56c48d170d7aa9c099751
Jeff Lunt <jefflunt@gmail.com>
2012-06-05 22:00:26 -0500

    References #41 - Added method for determining the highest sort order value on the Thoughts in a ThoughtWall.

--- app/models/thought_wall.rb
+++ app/models/thought_wall.rb
@@ -4,4 +4,8 @@ class ThoughtWall < ActiveRecord::Base
   validates :code,  :uniqueness => true
   validates :code,  :presence => true
   validates :title, :presence => true
+  
+  def highest_manual_order_value
+    thoughts.select("manual_order").order("manual_order DESC").limit(1).first.manual_order
+  end
 end
