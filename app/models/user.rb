89d0d5ea674380c071059fe7e5b8c8bd7bf71aeb
Jeff Lunt <jefflunt@gmail.com>
2012-06-24 13:34:37 -0500

    References #28 - Forced login provider uuid to be a string.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
-      user.uid = auth["uid"]
+      user.uid = auth["uid"].to_s
       
       case user.provider
       when "google_oauth2"
